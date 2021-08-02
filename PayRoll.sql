853e2497cad2d7643e3cb2f246f66cebe24f91e0
soubarnika-muthu <venkatachalapathymuthu@gmail.com>
2021-08-02 19:05:23 +0530

    [Soubarnika]Creating employee Payroll table

--- PayRoll.sql
+++ PayRoll.sql
@@ -1,2 +1,11 @@
 --UC1-Creating Database for the employee payroll  
 create database Emp_Payroll_Database
+
+--UC2-Creating table for employee payroll with attributes columns id,name, salary and start date
+create table PayRollTable
+(
+id int identity(1,1) primary key,
+name varchar(100) not null,
+salary float,
+startDate date
+)
