8d4b903768c792d8e3df188082b9dcf48cdca2be
soubarnika-muthu <venkatachalapathymuthu@gmail.com>
2021-08-02 20:29:47 +0530

    [Soubarnika]Checking UC4,5,6 for created table based on ER Diagram

--- PayRoll.sql
+++ PayRoll.sql
@@ -157,3 +157,5 @@ select sum(PayRoll.NetPay) as totalsalary,Employee.gender from PayRoll,Employee 
 select max(PayRoll.NetPay) as Minsalary,Employee.gender from PayRoll,Employee group by gender
 select min(PayRoll.NetPay) as Maxsalary,Employee.gender from PayRoll,Employee group by gender
 select avg(PayRoll.NetPay) as Avgsalary,Employee.gender from PayRoll,Employee group by gender
+
+select * from PayRoll
