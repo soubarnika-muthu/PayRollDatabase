5e54cba816dd8c533e5a31a223dad0d787b09bb1
soubarnika-muthu <venkatachalapathymuthu@gmail.com>
2021-08-02 19:39:06 +0530

    [Soubarnika]Retrieving data based on condition from table

--- PayRoll.sql
+++ PayRoll.sql
@@ -12,6 +12,12 @@ startDate date
 --UC3-Inserting the data into the table
 INSERT INTO PayRollTable VALUES('Soubarnika',10000,'2021-07-30')
 INSERT INTO PayRollTable VALUES('Geminika',50000,'2021-08-01')
+INSERT INTO PayRollTable VALUES('Shanthi',60000,'2021-06-11')
+INSERT INTO PayRollTable VALUES('venkat',40000,'2020-08-01')
 
 --UC4-Retriving the data from the table
 SELECT * FROM PayRollTable
+
+--UC5-Retriving data Based on condition
+SELECT salary FROM PayRollTable where name='venkat'
+SELECT salary FROM PayRollTable where startDate between CAST('2021-01-01' as date) and GETDATE()
