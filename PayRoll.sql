eaece2b7cd1bc262362a12d0318142bf97725a3e
soubarnika-muthu <venkatachalapathymuthu@gmail.com>
2021-08-02 19:33:20 +0530

    [Soubarnika]Retrieving data from the employee table

--- PayRoll.sql
+++ PayRoll.sql
@@ -12,3 +12,6 @@ startDate date
 --UC3-Inserting the data into the table
 INSERT INTO PayRollTable VALUES('Soubarnika',10000,'2021-07-30')
 INSERT INTO PayRollTable VALUES('Geminika',50000,'2021-08-01')
+
+--UC4-Retriving the data from the table
+SELECT * FROM PayRollTable
